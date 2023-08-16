53947808f7086eab3a9d75498ab23cc026563cca
Progdee <dhebhyofummite1@gmail.com>
2023-08-16 01:17:00 -0700

    tasks

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,12 @@
+# Define variables a and b
+a = 1
+b = 2
+
+#import the function add(a,b) from add_0.py
+from add_0 import add
+
+#calculate the result using the add function
+result = add(a,b)
+
+#print the formatted output
+print(f"(a) + (b) = {result}")
